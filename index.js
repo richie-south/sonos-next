05678d5bcdc956985693945d3a3607752b2e5d1a
richie-south <richard.soderman@gmail.com>
2019-04-16 18:25:22 +0200

    feat: add previous button

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { router, get } = require('microrouter')
 const { DeviceDiscovery, Sonos, SpotifyRegion } = require('sonos')
 const Gpio = require('onoff').Gpio
 const nextButton = new Gpio(2, 'in', 'both')
+const previousButton = new Gpio(2, 'in', 'both')
 
 const devices = []
 
@@ -19,6 +20,15 @@ const onNext = async () => {
   }))
 }
 
+const onPrevious = async () => {
+  await Promise.all(devices.map(async (device) => {
+    const sonos = new Sonos(device.host)
+    sonos.setSpotifyRegion(SpotifyRegion.EU)
+
+    await sonos.previous()
+  }))
+}
+
 nextButton.watch(async (err, value) => {
   if (err) {
     return console.log(err)
@@ -31,13 +41,29 @@ nextButton.watch(async (err, value) => {
   }
 })
 
+previousButton.watch(async (err, value) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  try {
+    await onPrevious()
+  } catch (error) {
+    console.error(error)
+  }
+})
+
 const onNextRoute = async (req, res) => {
   await onNext()
-  send(res, 200, {
-    hej: ':)'
-  })
+  send(res, 200, 'Next')
+}
+
+const onPreviousRoute = async (req, res) => {
+  await onPrevious()
+  send(res, 200, 'Previous')
 }
 
 module.exports = router(
-  get('/next', onNextRoute)
+  get('/next', onNextRoute),
+  get('/previous', onPreviousRoute)
 )
